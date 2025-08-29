1dc39bf76afe5364a83eff3f254f40b00f3e6293
Erik Weidenauer <erik.weidenauer@gmail.com>
2025-08-29 17:26:45 +0200

    adding new features
    - adding a better style to the main menu

--- Assets/MainMenu/Scripts/DanceManager.cs
+++ Assets/MainMenu/Scripts/DanceManager.cs
@@ -4,7 +4,10 @@ using UnityEngine.UIElements;
 
 public class UIManager : MonoBehaviour
 {
+    private VisualElement myDancesContainer;
     private Button myDances;
+    
+    private VisualElement onlineDancesContainer;
     private Button onlineDances;
     private ScrollView danceContainer;
     
@@ -16,19 +19,25 @@ public class UIManager : MonoBehaviour
         var uiDoc = FindObjectOfType<UIDocument>();
         var root = uiDoc.rootVisualElement;
         
+        myDancesContainer = root.Q<VisualElement>("myDancesContainer");
         myDances = root.Q<Button>("myDances");
         myDances.clicked += FetchMyDances;
         
+        onlineDancesContainer = root.Q<VisualElement>("onlineDancesContainer");
         onlineDances = root.Q<Button>("onlineDances");
         onlineDances.clicked += FetchOnlineDances;
         
         danceContainer = root.Q<ScrollView>("danceContainer");
+        
+        FetchMyDances();
     }
 
     private void FetchMyDances()
     {
         danceContainer.Clear();
-
+        onlineDancesContainer.RemoveFromClassList("activeDancesContainer");
+        myDancesContainer.AddToClassList("activeDancesContainer");
+        
         foreach (var dance in myDanceList)
         {
             var btn = new Button();
@@ -49,6 +58,8 @@ public class UIManager : MonoBehaviour
     private void FetchOnlineDances()
     {
         danceContainer.Clear();
+        myDancesContainer.RemoveFromClassList("activeDancesContainer");
+        onlineDancesContainer.AddToClassList("activeDancesContainer");
         
         foreach (var dance in onlineDanceList)
         {
